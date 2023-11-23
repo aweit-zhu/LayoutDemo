3d0060f6502edfc163f840e536b711f078a9720d
aweit <aa4192696@gmail.com.tw>
2023-11-23 15:19:28 +0800

    update

--- src/main/test/BCryptExample.java
+++ src/main/test/BCryptExample.java
@@ -5,9 +5,9 @@ public class BCryptExample {
 	public static void main(String[] args) {
 
         // 加密密码
-        String rawPassword = "123";
-        String hashedPassword = BCrypt.hashpw(rawPassword, BCrypt.gensalt()); 
-        System.out.println("Hashed Password: " + hashedPassword); // $2a$10$qgVZMdNoKQ/JwXr4eQS8SOu2cYBPE9Y.ox2QRX9y.wjIKQZ7S3OBe
+        //String rawPassword = "123";
+        //String hashedPassword = BCrypt.hashpw(rawPassword, BCrypt.gensalt()); 
+        //System.out.println("Hashed Password: " + hashedPassword); // $2a$10$qgVZMdNoKQ/JwXr4eQS8SOu2cYBPE9Y.ox2QRX9y.wjIKQZ7S3OBe
 
         // 驗證密碼
         String userPassword = "123"; // 模擬用戶輸入的密碼(明碼)
